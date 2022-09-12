369aa689e07201aefa14ec163e3a210667b53f39
Pavel Evtushenko <whatisclamp@gmail.com>
2022-09-12 10:52:54 +0300

    feat: add routing. Add news, music and settings component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import'./App.css';
+import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
-        <div className='app_wrapper'>
-            <Header/>
-            <Navbar/>
-            {/*<div className='app_wrapper_content'>*/}
-            {/*    <Profile/>*/}
-            {/*</div>*/}
-            <div className='app_wrapper_content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app_wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app_wrapper_content'>
+                    <Routes>
+                        <Route element={<Dialogs/>} path='/dialogs'/>
+                        <Route element={<Profile/>} path='/profile' />
+                        <Route element={<News/>} path='/news' />
+                        <Route element={<Music/>} path='/music' />
+                        <Route element={<Settings/>} path='/settings' />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,19 +4,19 @@ const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={`${classes.item}`}>
-                <a href="src/components/Navbar/Navbar#s">Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={classes.item}>
-                <a href="src/components/Navbar/Navbar#s">Messages</a>
+                <a href="/dialogs">Messages</a>
             </div>
             <div className={classes.item}>
-                <a href="src/components/Navbar/Navbar#s">News</a>
+                <a href="/news">News</a>
             </div>
             <div className={classes.item}>
-                <a href="src/components/Navbar/Navbar#s">Music</a>
+                <a href="/music">Music</a>
             </div>
             <div className={classes.item}>
-                <a href="src/components/Navbar/Navbar#s">Settings</a>
+                <a href="/settings">Settings</a>
             </div>
         </nav>
     );
